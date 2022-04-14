ac42bcde7f6902d71f4693da79fbc555baf3b572
Elvin <Elvin@ElvinGarcia.com>
2022-04-14 08:56:31 -0400

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './App';
+import manageUsers from './reducers/manageUsers';
+import {Provider} from 'react-redux'
 
-import manageUsers from './reducers/manageUsers'
-
-
-// add imports and code
 
 
 ReactDOM.render(
-  // add imports and code
+  <Provider>
   <App />
-  // add imports and code
+  </Provider>
   ,
   document.getElementById('root')
 );
