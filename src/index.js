48dad0e3931f4fe2bc5dc739d16c471833d205b1
Elvin <Elvin@ElvinGarcia.com>
2022-04-14 23:10:43 -0400

    done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import manageUsers from './reducers/manageUsers';
-import { Provider } from 'react-redux';
 import {createStore} from 'redux'
+import { Provider } from 'react-redux';
 
-const store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+let store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 ReactDOM.render(
